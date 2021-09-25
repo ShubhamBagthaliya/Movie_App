bd476ccb407a8928d165048d459393df6487da25
Vivek <shineinfosoft32@gmail.com>
2021-09-25 17:08:59 +0530

    Commit

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { img_300, unavailable } from "../../config/config";
+
+const SingleContent = ({
+    id,
+    poster,
+    title,
+    date,
+    media_type,
+    vote_average
+}) => {
+    return (
+        <div>
+            <img src={poster ? `${img_300}/${poster}` : unavailable } alt={title} />
+            <b className="title">{title}</b>
+            <span>
+                {media_type === 'tv' ? 'TV Series' : "Movie"}
+            </span>
+        </div>
+    )
+}
+
+export default SingleContent
